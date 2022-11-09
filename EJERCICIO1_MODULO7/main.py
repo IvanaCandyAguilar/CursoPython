5bdbf9c3dcf536720d29aecfd43ce49babcffa40
Ivana Candy <93547639+IvanaCandyAguilar@users.noreply.github.com>
2022-11-09 11:52:36 -0400

    Add files via upload

---
+++ EJERCICIO1_MODULO7/main.py
@@ -0,0 +1,16 @@
+import operaciones as op
+
+def main():
+    print("suma",op.suma(1,5,1,5,2))
+    print("resta",op.resta(1,5,1,5,2))
+    print("multiplicacion",op.multiplicacion(5,2))
+    print("division",op.division(25,3))
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
---
+++ EJERCICIO1_MODULO7/operaciones.py
@@ -0,0 +1,17 @@
+def suma(*args):
+    resultado=0
+    for arg in args:
+        resultado += arg
+    return resultado
+
+def resta(*args):
+    resultado=0
+    for arg in args:
+        resultado -= arg
+    return resultado
+
+def multiplicacion(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
